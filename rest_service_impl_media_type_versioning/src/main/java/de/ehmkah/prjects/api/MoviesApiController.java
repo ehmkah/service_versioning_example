53c056f79b853500639de5d898e975b2dd3086af
Michael Krausse <michael.krausse@directbox.com>
2017-08-04 18:07:36 +0200

    renamed rest-implementation because it is a for media_type_versioning

---
+++ rest_service_impl_media_type_versioning/src/main/java/de/ehmkah/prjects/api/MoviesApiController.java
@@ -0,0 +1,32 @@
+package de.ehmkah.prjects.api;
+
+import io.swagger.annotations.ApiParam;
+import io.swagger.model.Movie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/movies")
+public class MoviesApiController {
+
+  @RequestMapping(method = RequestMethod.GET, produces = "application/vnd.ehmkah.app-1.0+json")
+  public ResponseEntity<List<Movie>> findMovies(
+          @ApiParam(value = "Tags used to filter the result")
+          @RequestParam(value = "tags", required = false) List<String> tags,
+          @ApiParam(value = "maximum number of results to return") @RequestParam(value = "limit", required = false) Integer limit) {
+    List<Movie> movies = new ArrayList<>();
+    Movie movie = new Movie();
+    movie.setTitle("HUHU, version 1.0");
+    movies.add(movie);
+    ResponseEntity<List<Movie>> result = ResponseEntity.ok(movies);
+
+    return result;
+  }
+
+}
--- rest_service_impl_media_type_versioning/src/main/java/de/ehmkah/prjects/api/MoviesApiControllerV2.java
+++ rest_service_impl_media_type_versioning/src/main/java/de/ehmkah/prjects/api/MoviesApiControllerV2.java
@@ -13,21 +13,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/movies")
-public class MoviesApiController implements MoviesApi {
-
-  @RequestMapping(method = RequestMethod.GET, produces = "application/vnd.ehmkah.app-1.0+json")
-  public ResponseEntity<List<Movie>> findMovies(
-          @ApiParam(value = "Tags used to filter the result")
-          @RequestParam(value = "tags", required = false) List<String> tags,
-          @ApiParam(value = "maximum number of results to return") @RequestParam(value = "limit", required = false) Integer limit) {
-    List<Movie> movies = new ArrayList<>();
-    Movie movie = new Movie();
-    movie.setTitle("HUHU, version 1.0");
-    movies.add(movie);
-    ResponseEntity<List<Movie>> result = ResponseEntity.ok(movies);
-
-    return result;
-  }
+public class MoviesApiControllerV2 {
 
   @RequestMapping(method = RequestMethod.GET, produces = "application/vnd.ehmkah.app-2.0+json")
   public ResponseEntity<List<Movie>> findMovies2(
