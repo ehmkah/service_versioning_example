ef2e0eacddea5be069e95ea5c71c97da157f2869
Michael Krausse <michael.krausse@directbox.com>
2017-08-09 14:25:05 +0200

    * modelmapper dummy implementation * think about testing your service endpoints

---
+++ soap_service_impl_adapter_modelmapper/src/main/java/de/ehmkah/projects/service_example/soap_service/ConverterV1ToCurrent.java
@@ -0,0 +1,25 @@
+package de.ehmkah.projects.service_example.soap_service;
+
+import com.ehmkah.services.gardening.ResponseType;
+import de.ehmkah.projects.service_versioning_example.soap.v1.RequestType;
+import org.modelmapper.ModelMapper;
+
+/**
+ * @author Michael Krausse (ehmkah)
+ */
+public class ConverterV1ToCurrent {
+
+  private ModelMapper modelMapper = new ModelMapper();
+
+  public com.ehmkah.services.gardening.RequestType map(RequestType requestV1) {
+    com.ehmkah.services.gardening.RequestType result = modelMapper.map(requestV1, com.ehmkah.services.gardening.RequestType.class);
+
+    return result;
+  }
+
+  public de.ehmkah.projects.service_versioning_example.soap.v1.ResponseType map(ResponseType responseTypeCurrent) {
+    de.ehmkah.projects.service_versioning_example.soap.v1.ResponseType result = modelMapper.map(responseTypeCurrent, de.ehmkah.projects.service_versioning_example.soap.v1.ResponseType.class);
+
+    return result;
+  }
+}
