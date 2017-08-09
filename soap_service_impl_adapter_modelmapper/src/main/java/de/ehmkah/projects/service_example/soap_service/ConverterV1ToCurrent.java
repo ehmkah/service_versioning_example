7148452a2be832cf23855c88595b1d20c2239df9
Michael Krausse <michael.krausse@directbox.com>
2017-08-09 14:35:22 +0200

    * modelmapper converter is used

--- soap_service_impl_adapter_modelmapper/src/main/java/de/ehmkah/projects/service_example/soap_service/ConverterV1ToCurrent.java
+++ soap_service_impl_adapter_modelmapper/src/main/java/de/ehmkah/projects/service_example/soap_service/ConverterV1ToCurrent.java
@@ -3,10 +3,12 @@ package de.ehmkah.projects.service_example.soap_service;
 import com.ehmkah.services.gardening.ResponseType;
 import de.ehmkah.projects.service_versioning_example.soap.v1.RequestType;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Michael Krausse (ehmkah)
  */
+@Component
 public class ConverterV1ToCurrent {
 
   private ModelMapper modelMapper = new ModelMapper();
--- soap_service_impl_adapter_modelmapper/src/main/java/de/ehmkah/projects/service_example/soap_service/GardeningServiceV1Impl.java
+++ soap_service_impl_adapter_modelmapper/src/main/java/de/ehmkah/projects/service_example/soap_service/GardeningServiceV1Impl.java
@@ -1,6 +1,5 @@
 package de.ehmkah.projects.service_example.soap_service;
 
-import de.ehmkah.projects.service_example.soap_service.domain.NeigbhbourCheckService;
 import de.ehmkah.projects.service_versioning_example.soap.v1.RequestType;
 import de.ehmkah.projects.service_versioning_example.soap.v1.ResponseType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +10,19 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 public class GardeningServiceV1Impl {
 
   @Autowired
-  private NeigbhbourCheckService neigbhbourCheckService;
+  private ConverterV1ToCurrent converterV1ToCurrent;
+
+  @Autowired
+  private GardeningServiceImpl gardeningService;
 
   private static final String NAMESPACE_URI = "http://ehmkah.com/services/gardening";
 
   @PayloadRoot(namespace = NAMESPACE_URI, localPart = "checkNeighbours")
   @ResponsePayload
   public ResponseType checkNeighbours(@RequestPayload RequestType parameters) {
-    ResponseType result = new ResponseType();
-
-    String checkResult = neigbhbourCheckService.areGood(parameters.getSpeciesOne(), parameters.getSpeciesTwo());
-    result.setAreGoodNeighbours(checkResult);
+    com.ehmkah.services.gardening.RequestType requestCurrent = converterV1ToCurrent.map(parameters);
+    com.ehmkah.services.gardening.ResponseType responseTypeCurrent = gardeningService.checkNeighbours(requestCurrent);
+    ResponseType result = converterV1ToCurrent.map(responseTypeCurrent);
 
     return result;
   }
