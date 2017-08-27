6fd8274b9e6c1ef07386eb0c52082a0413f7a042
Michael Krausse <michael.krausse@directbox.com>
2017-08-27 12:50:04 +0200

    * use cxf for v1 impl too

--- soap_service_impl_v1/src/main/java/de/ehmkah/projects/service_example/soap_service/GardeningServiceImpl.java
+++ soap_service_impl_v1/src/main/java/de/ehmkah/projects/service_example/soap_service/GardeningServiceImpl.java
@@ -2,33 +2,44 @@ package de.ehmkah.projects.service_example.soap_service;
 
 
 import de.ehmkah.projects.service_example.soap_service.domain.NeigbhbourCheckService;
+import de.ehmkah.projects.service_versioning_example.soap.v1.ObjectFactory;
 import de.ehmkah.projects.service_versioning_example.soap.v1.RequestType;
 import de.ehmkah.projects.service_versioning_example.soap.v1.ResponseType;
+import de.ehmkah.projects.service_versioning_example.soap.v1.Wsgardening;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.ws.server.endpoint.annotation.Endpoint;
-import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
-import org.springframework.ws.server.endpoint.annotation.RequestPayload;
-import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.bind.annotation.XmlSeeAlso;
 
 
-@Endpoint
 @Component
-public class GardeningServiceImpl {
+@WebService(name = "Wsgardening", targetNamespace = "http://ehmkah.com/services/gardening", serviceName = "Wsgardening")
+@SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE)
+@XmlSeeAlso({
+        ObjectFactory.class
+})
+public class GardeningServiceImpl implements Wsgardening {
 
   @Autowired
-  private NeigbhbourCheckService neigbhbourCheckService;
+  private NeigbhbourCheckService internalService;
 
   private static final String NAMESPACE_URI = "http://ehmkah.com/services/gardening";
 
-  @PayloadRoot(namespace = NAMESPACE_URI, localPart = "request")
-  @ResponsePayload
-  public ResponseType checkNeighbours(@RequestPayload RequestType parameters) {
+  @Override
+  @WebMethod(action = "http://ehmkah.com/services/gardening/#checkNeighbours")
+  @WebResult(name = "responseType", targetNamespace = "http://ehmkah.com/services/gardening", partName = "response")
+  public ResponseType checkNeighbours(
+          @WebParam(name = "requestType", targetNamespace = "http://ehmkah.com/services/gardening", partName = "request")
+                  RequestType parameters) {
+
+    String areGoodNeigbours = internalService.areGood(parameters.getSpeciesOne(), parameters.getSpeciesTwo());
     ResponseType result = new ResponseType();
-
-    String checkResult = neigbhbourCheckService.areGood(parameters.getSpeciesOne(), parameters.getSpeciesTwo());
-    result.setAreGoodNeighbours(checkResult);
-
+    result.setAreGoodNeighbours(areGoodNeigbours);
     return result;
   }
 }
--- soap_service_impl_v1/src/main/java/de/ehmkah/projects/service_example/soap_service/WebServiceConfiguration.java
+++ soap_service_impl_v1/src/main/java/de/ehmkah/projects/service_example/soap_service/WebServiceConfiguration.java
@@ -1,14 +1,16 @@
 package de.ehmkah.projects.service_example.soap_service;
 
+import de.ehmkah.projects.service_versioning_example.soap.v1.Wsgardening;
+import org.apache.cxf.bus.spring.SpringBus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.apache.cxf.transport.servlet.CXFServlet;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
-import org.springframework.ws.transport.http.MessageDispatcherServlet;
-import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
-import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
+
+import javax.xml.ws.Endpoint;
 
 /**
  * @author Michael Krausse (ehmkah)
@@ -16,20 +18,29 @@ import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
 @EnableWs
 @Configuration
 public class WebServiceConfiguration {
-  
-  @Bean
-  public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
-    MessageDispatcherServlet servlet = new MessageDispatcherServlet();
-    servlet.setApplicationContext(applicationContext);
-    servlet.setTransformWsdlLocations(true);
-    return new ServletRegistrationBean(servlet, "/ws/*");
+
+  @Autowired
+  private Wsgardening gardeningServiceV1;
+
+  @Bean(name = "cxf")
+  public SpringBus springBus() {
+    return new SpringBus();
   }
 
-  @Bean(name = "service")
-  public Wsdl11Definition defaultWsdl11Definition() {
-    SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
-    wsdl11Definition.setWsdl(new ClassPathResource("/wsdl/serviceDefinitionV1.wsdl"));
+  @Bean("endpointServiceV1")
+  public Endpoint endpointServiceV1(SpringBus springBus) {
+    EndpointImpl result = new EndpointImpl(springBus, gardeningServiceV1);
+    result.setWsdlLocation("classpath:/wsdl/serviceDefinitionV1.wsdl");
+    result.publish("/v1/service");
 
-    return wsdl11Definition;
+    return result;
+  }
+
+  @Bean
+  public ServletRegistrationBean servletRegistrationBean() {
+    String url = "/overview/*";
+    ServletRegistrationBean result = new ServletRegistrationBean(new CXFServlet(), url);
+    result.setLoadOnStartup(1);
+    return result;
   }
 }
