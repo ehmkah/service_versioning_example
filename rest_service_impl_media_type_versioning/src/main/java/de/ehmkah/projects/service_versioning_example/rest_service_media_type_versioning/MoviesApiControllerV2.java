f606cce46b647ee124dae0c3e02fe2d5c4a89c5a
Michael Krausse <michael.krausse@directbox.com>
2017-08-28 22:25:48 +0200

    simplified

--- rest_service_impl_media_type_versioning/src/main/java/de/ehmkah/projects/service_versioning_example/rest_service_media_type_versioning/MoviesApiControllerV2.java
+++ rest_service_impl_media_type_versioning/src/main/java/de/ehmkah/projects/service_versioning_example/rest_service_media_type_versioning/MoviesApiControllerV2.java
@@ -1,12 +1,10 @@
 package de.ehmkah.projects.service_versioning_example.rest_service_media_type_versioning;
 
 import de.ehmkah.v2.Movie;
-import io.swagger.annotations.ApiParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,10 +14,7 @@ import java.util.List;
 public class MoviesApiControllerV2 {
 
   @RequestMapping(method = RequestMethod.GET, produces = "application/vnd.ehmkah.app-2.0+json")
-  public ResponseEntity<List<Movie>> findMovies2(
-          @ApiParam(value = "Tags used to filter the result")
-          @RequestParam(value = "tags", required = false) List<String> tags,
-          @ApiParam(value = "maximum number of results to return") @RequestParam(value = "limit", required = false) Integer limit) {
+  public ResponseEntity<List<Movie>> findMovies() {
     List<Movie> movies = new ArrayList<>();
     Movie movie = new Movie();
     movie.setTitle("HUHU, version 2.0");
